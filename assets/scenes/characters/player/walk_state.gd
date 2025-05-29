33d38c56015d6b18ae31a28467e4bb9c2ac34262
hfaso <liujunli137@163.com>
2025-05-29 20:53:42 +0800

    003.2

--- assets/scenes/characters/player/walk_state.gd
+++ assets/scenes/characters/player/walk_state.gd
@@ -2,31 +2,31 @@ extends NodeState
 
 @export var player: CharacterBody2D
 @export var animated_sprite_2d: AnimatedSprite2D
-
-var direction: Vector2
+@export var speed: int = 50
 
 func _on_process(_delta : float) -> void:
-	pass 
+	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	direction = GameInputEvents.movement_input()
-	
+	var direction: Vector2 = GameInputEvents.movement_input()
+
 	if direction == Vector2.UP:
-		animated_sprite_2d.play("idle_back")
+		animated_sprite_2d.play("walk_back")
 	elif direction == Vector2.DOWN:
-		animated_sprite_2d.play("idle_front")	
+		animated_sprite_2d.play("walk_front")
 	elif direction == Vector2.LEFT:
-		animated_sprite_2d.play("idle_left")
+		animated_sprite_2d.play("walk_left")
 	elif direction == Vector2.RIGHT:
-		animated_sprite_2d.play("idle_right")
-	else:
-		animated_sprite_2d.play("idle_front")
+		animated_sprite_2d.play("walk_right")
 		
+	player.velocity = direction * speed
+	player.move_and_slide()
+
 func _on_next_transitions() -> void:
-	GameInputEvents.movement_input()
-	if GameInputEvents.is_movement_input():
-		transition.emit("Walk")
+	if !GameInputEvents.is_movement_input():
+		transition.emit("Idle")
+	pass
 
 
 func _on_enter() -> void:
