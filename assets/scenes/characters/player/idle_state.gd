2a1a42a5ccd83a1a2d93e3819f2ce6bd9c464f0a
june <june.liu@yuansuan.cn>
2025-06-01 18:08:19 +0800

    004.01

--- assets/scenes/characters/player/idle_state.gd
+++ assets/scenes/characters/player/idle_state.gd
@@ -1,24 +1,21 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
-
-var direction: Vector2
 
 func _on_process(_delta : float) -> void:
 	pass 
 
 
 func _on_physics_process(_delta : float) -> void:
-	direction = GameInputEvents.movement_input()
 	
-	if direction == Vector2.UP:
+	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("idle_back")
-	elif direction == Vector2.DOWN:
+	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("idle_front")	
-	elif direction == Vector2.LEFT:
+	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("idle_left")
-	elif direction == Vector2.RIGHT:
+	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("idle_right")
 	else:
 		animated_sprite_2d.play("idle_front")
@@ -27,6 +24,8 @@ func _on_next_transitions() -> void:
 	GameInputEvents.movement_input()
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
+	if player.current_tool == DataTypes.Tools.AxeWood:
+		transition.emit("Chopping")
 
 
 func _on_enter() -> void:
@@ -34,4 +33,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
