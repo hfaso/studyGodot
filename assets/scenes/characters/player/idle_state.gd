6372b5b91cdda6130d10043f8408c7ab7ffca82b
june <june.liu@yuansuan.cn>
2025-06-08 12:24:17 +0800

    004finish

--- assets/scenes/characters/player/idle_state.gd
+++ assets/scenes/characters/player/idle_state.gd
@@ -24,9 +24,12 @@ func _on_next_transitions() -> void:
 	GameInputEvents.movement_input()
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
-	if player.current_tool == DataTypes.Tools.AxeWood:
+	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
 		transition.emit("Chopping")
-
+	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("Tilling")
+	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+		transition.emit("Watering")
 
 func _on_enter() -> void:
 	pass
